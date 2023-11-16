224de6544544b3a3643278a863499ef339280e75
YOUDA <4B0G0137@stust.edu.tw>
2023-11-16 13:23:54 +0800

    2.0

--- Homework06.py
+++ Homework06.py
@@ -1,10 +1,14 @@
 class MyShape:
 
-    def __init__(self, square_side, length, width, radius):
+    def __init__(self, name, square_side, length, width, radius):
+        self.name = name
         self.side = square_side
         self.length = length
         self.width = width
         self.radius = radius
+
+    def __str__(self):
+        return f"Name : {self.name}\nsquare_area : {self.getSquareArea()}\nrectangle_area : {self.getRectangleArea()}\ncircle_area : {self.getCircleArea()}\n\n"
 
     def getSquareArea(self):
         return pow(self.side, 2)
@@ -16,11 +20,10 @@ class MyShape:
         return pow(self.radius, 2) * 3.14
 
 
-person = MyShape(10, 175, 50, 100)
-person_square_area = person.getSquareArea()
-person_rectangle_area = person.getRectangleArea()
-person_circle_area = person.getCircleArea()
-
-print(
-    f"square_area : {person_square_area}\nrectangle_area : {person_rectangle_area}\ncircle_area : {person_circle_area}"
-)
+#test data
+person01 = MyShape("Eddie", 10, 175, 50, 100)
+person02 = MyShape("John", 25, 165, 40, 90)
+person03 = MyShape("Mary", 5, 150, 35, 60)
+print(person01)
+print(person02)
+print(person03)
